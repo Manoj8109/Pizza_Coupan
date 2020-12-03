ee563fb9f75979e9b72dbb4089d65c3aec5d0ef0
Manoj <vtu8109@veltechuniv.edu.in>
2020-12-03 18:11:42 +0530

    Coupan

--- OnlinePizzaOrderingApplication/src/main/java/com/onlinepizza/model/Coupan.java
+++ OnlinePizzaOrderingApplication/src/main/java/com/onlinepizza/model/Coupan.java
@@ -1,7 +1,6 @@
 package com.onlinepizza.model;
 
 import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,50 +8,53 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
-import com.sun.istack.NotNull;
 @Entity
-@Table(name="Coupan1")
-@SequenceGenerator(name="sequence", initialValue = 1, allocationSize = 1)
-public class Coupan implements Serializable{
+@Table(name = "Coupan1")
+@SequenceGenerator(name = "sequence", initialValue = 1, allocationSize = 1)
+public class Coupan implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence")
-	@Column(name="COUPANID",length=10)
+	@Column(name = "COUPANID", length = 10)
 	private Integer coupanId;
 	@Column(length = 18)
-	@NotNull
+	@NotBlank(message = "CoupanName may not be null")
 	private String coupanName;
 	@Column(length = 35)
+	@NotBlank(message = "CoupanType may not be null")
 	private String coupanType;
 	@Column(length = 60)
+	@NotBlank(message = "CoupanDescription may not be null")
 	private String coupanDescription;
 	@Column(length = 5)
-	@NotNull
+	@NotNull(message = "CoupanPizzaId may not be null")
 	private Integer coupanPizzaId;
-	
+
 	/**
 	 * Default constructor
 	 */
-	
+
 	public Coupan() {
-		
+
 	}
-	
+
 	/**
-	 * Parameterized constructor 
-	 * @param coupanId
+	 * Parameterized constructor
 	 * @param coupanName
+	 * @param coupanType
 	 * @param coupanDescription
-	 * @param coupanPizzaId
 	 */
+
 	public Coupan(String coupanName, String coupanType, String coupanDescription) {
 		super();
 		this.coupanName = coupanName;
 		this.coupanType = coupanType;
 		this.coupanDescription = coupanDescription;
 	}
-	
+
 	/**
 	 * 
 	 * Getters and Setters
@@ -60,42 +62,50 @@ public class Coupan implements Serializable{
 	public Integer getCoupanId() {
 		return coupanId;
 	}
+
 	public void setCoupanId(Integer coupanId) {
 		this.coupanId = coupanId;
 	}
+
 	public String getCoupanName() {
 		return coupanName;
 	}
+
 	public void setCoupanName(String coupanName) {
 		this.coupanName = coupanName;
 	}
+
 	public String getCoupanType() {
 		return coupanType;
 	}
+
 	public void setCoupanType(String coupanType) {
 		this.coupanType = coupanType;
 	}
+
 	public String getCoupanDescription() {
 		return coupanDescription;
 	}
+
 	public void setCoupanDescription(String coupanDescription) {
 		this.coupanDescription = coupanDescription;
 	}
+
 	public Integer getCoupanPizzaId() {
 		return coupanPizzaId;
 	}
+
 	public void setCoupanPizzaId(Integer coupanPizzaId) {
 		this.coupanPizzaId = coupanPizzaId;
-	}	
-	
+	}
+
 	/**
 	 * to String method
 	 */
 	@Override
 	public String toString() {
-	return "Coupan [coupanId=" + coupanId + ", coupanName=" + coupanName + ", coupanType=" + coupanType
+		return "Coupan [coupanId=" + coupanId + ", coupanName=" + coupanName + ", coupanType=" + coupanType
 				+ ", coupanDescription=" + coupanDescription + ", coupanPizzaId=" + coupanPizzaId + "]";
-}
-	
-	
+	}
+
 }
