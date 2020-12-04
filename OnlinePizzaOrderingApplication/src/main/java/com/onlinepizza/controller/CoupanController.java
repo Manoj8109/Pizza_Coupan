ff570ad453146dc25ad3369142f5e693a3bb9260
Manoj <vtu8109@veltechuniv.edu.in>
2020-12-04 09:05:03 +0530

    completed

--- OnlinePizzaOrderingApplication/src/main/java/com/onlinepizza/controller/CoupanController.java
+++ OnlinePizzaOrderingApplication/src/main/java/com/onlinepizza/controller/CoupanController.java
@@ -3,6 +3,8 @@ package com.onlinepizza.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -17,8 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.onlinepizza.exceptions.CoupanIdNotFoundException;
-import com.onlinepizza.exceptions.GlobalPizzaExceptionHandler;
 import com.onlinepizza.model.Coupan;
 import com.onlinepizza.service.ICoupanService;
 
@@ -30,16 +30,14 @@ public class CoupanController {
 	ICoupanService service;
 
 	@PostMapping("/create") // http://localhost:1205/coupans/create
-	public ResponseEntity<Boolean> addCoupan(@RequestBody Coupan coupan) {
+	public ResponseEntity<Boolean> addCoupan(@Valid @RequestBody Coupan coupan) {
 		service.addCoupan(coupan);
-		@SuppressWarnings({ "rawtypes", "unchecked" })
 		ResponseEntity<Boolean> responseEntity = new ResponseEntity<Boolean>(true, HttpStatus.OK);
 		return responseEntity;
 	}
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@PutMapping("/update") // http://localhost:1205/coupans/update
-	public ResponseEntity<Boolean> editCoupan(@RequestBody Coupan coupan) {
+	public ResponseEntity<Boolean> editCoupan(@Valid @RequestBody Coupan coupan) {
 		coupan = service.editCoupan(coupan);
 		ResponseEntity<Boolean> responseEntity = new ResponseEntity<Boolean>(true, HttpStatus.OK);
 		return responseEntity;
@@ -62,10 +60,4 @@ public class CoupanController {
 		service.deleteCoupan(id);
 		return "Deleted Coupan";
 	}
-	
-
 }
-
-/*
- * get(Fetch) post(Insert) delete put(Update)
- */
--- OnlinePizzaOrderingApplication/src/main/java/com/onlinepizza/exceptions/GlobalPizzaExceptionHandler.java
+++ OnlinePizzaOrderingApplication/src/main/java/com/onlinepizza/exceptions/GlobalPizzaExceptionHandler.java
@@ -1,21 +1,44 @@
 package com.onlinepizza.exceptions;
 
+import java.time.LocalDate;
 import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.onlinepizza.service.CoupanServiceImpl;
 
 @ControllerAdvice
-public class GlobalPizzaExceptionHandler {
+public class GlobalPizzaExceptionHandler extends ResponseEntityExceptionHandler{
 	Logger logger=LoggerFactory.getLogger(CoupanServiceImpl.class);
 	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		Map<String, Object> body = new LinkedHashMap<>();
+		body.put("timestamp", LocalDate.now());
+		body.put("status", status.value());
+
+		List<String> errors = ex.getBindingResult().getFieldErrors().stream().map(x -> x.getDefaultMessage())
+				.collect(Collectors.toList());
+
+		body.put("errors", errors);
+
+		return new ResponseEntity<>(body, headers, status);
+	}
 	@ExceptionHandler(CoupanIdNotFoundException.class)
 	public ResponseEntity<?>CoupanIdNotFoundException(CoupanIdNotFoundException e,WebRequest request){
 		ErrorDetails errorDetails = new ErrorDetails(new Date(),e.getMessage(), request.getDescription(false));
